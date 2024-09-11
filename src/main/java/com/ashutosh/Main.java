b2048aa6735224ca63279d09c4e1fdad865d823a
Ashutosh Sanghi <ashutoshsanghi3@gmail.com>
2024-09-11 15:10:51 +0530

    specify main file in gradle
    Signed-off-by: Ashutosh Sanghi <ashutoshsanghi3@gmail.com>

--- src/main/java/com/ashutosh/Main.java
+++ src/main/java/com/ashutosh/Main.java
@@ -4,5 +4,6 @@ package com.ashutosh;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
