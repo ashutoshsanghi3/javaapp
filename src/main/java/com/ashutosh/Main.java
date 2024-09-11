d39f74909b7812936f6020cfaa918577d068a236
Ashutosh Sanghi <ashutoshsanghi3@gmail.com>
2024-09-11 18:51:33 +0530

    adding http request using okhttp library
    Signed-off-by: Ashutosh Sanghi <ashutoshsanghi3@gmail.com>

--- src/main/java/com/ashutosh/Main.java
+++ src/main/java/com/ashutosh/Main.java
@@ -1,19 +1,39 @@
 package com.ashutosh;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Main {
+    static OkHttpClient client = new OkHttpClient();
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Person p1 = Person.builder().name("Ram Kumar").age(19).build();
         Person p2 = Person.builder().name("Ram Kumar").age(19).build();
-
-        System.out.println(p1.getAge());
-        System.out.println(p2.getName());
         //Due to @EqualsAndHashCode ,p1 and p2 will have the same hash if both have same age and name
-        System.out.println(p1.toString());
+        System.out.println(p1.toString()); //@Data doesn't produce the @EqualsAndHashCode effect
         System.out.println(p2.toString());
+
         System.out.println(p1.equals(p2)); //true because matches hash code
         System.out.println(p1==p2); //false because it compares memory locations
+
+        try{
+            System.out.println(getUrl("https://example.com/"));
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    static String getUrl(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 }
